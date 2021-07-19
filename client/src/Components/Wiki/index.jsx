e261a61304a2f73d7bf15224159c8ea42fcc4343
sonicsmith <thesonicsmith@gmail.com>
2021-07-19 21:21:15 +1200

    Fix wiki page

--- client/src/Components/Wiki/index.jsx
+++ client/src/Components/Wiki/index.jsx
@@ -3,7 +3,10 @@ import { Box, Text, Heading } from "grommet"
 export default () => {
   return (
     <Box alignContent="center" fill>
-      <iframe src="https://thesonicsmith.gitbook.io/crypto-city/"></iframe>
+      <iframe
+        src="https://thesonicsmith.gitbook.io/crypto-city/"
+        style={{ height: "100%" }}
+      ></iframe>
     </Box>
   )
 }
