4a1e3e9058a8956f7515944ff4676fc23757d31a
sonicsmith <thesonicsmith@gmail.com>
2021-07-20 20:13:28 +1200

    Improved Wiki style

--- client/src/Components/Wiki/index.jsx
+++ client/src/Components/Wiki/index.jsx
@@ -3,6 +3,7 @@ import { Box, Text, Heading } from "grommet"
 export default () => {
   return (
     <Box alignContent="center" fill>
+      <Box pad={{ bottom: "large" }} />
       <iframe
         src="https://thesonicsmith.gitbook.io/crypto-city/"
         style={{ height: "100%" }}
